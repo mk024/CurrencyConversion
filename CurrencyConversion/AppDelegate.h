3a14023069bfd6ee33bbd16fef5d5f542750bb7c
Michael K <mk024@pro.hsd1.il.comcast.net>
2016-08-08 14:57:03 -0500

    Initial Commit

---
+++ CurrencyConversion/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CurrencyConversion
+//
+//  Created by Michael K on 8/8/16.
+//  Copyright © 2016 Waxy Watermelon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CurrencyConversion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CurrencyConversion
+//
+//  Created by Michael K on 8/8/16.
+//  Copyright © 2016 Waxy Watermelon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
