25e32236867f2393ba4199ecb7964e22fa74a678
Michael Kathan <mk024@comcast.net>
2016-09-20 13:09:35 -0500

    Currency Conversion
    Currency Conversion app for iOS built with Objective C

--- CurrencyConversion/ViewController.m
+++ CurrencyConversion/ViewController.m
@@ -7,21 +7,67 @@
 //
 
 #import "ViewController.h"
+#import "CurrencyRequest/CRCurrencyRequest.h"
+#import "CurrencyRequest/CRCurrencyResults.h"
 
-@interface ViewController ()
+@interface ViewController () <CRCurrencyRequestDelegate>
+
+@property (nonatomic) CRCurrencyRequest *req;
+@property (weak, nonatomic) IBOutlet UITextField *inputField;
+@property (weak, nonatomic) IBOutlet UIButton *convertButton;
+@property (weak, nonatomic) IBOutlet UILabel *currencyA;
+@property (weak, nonatomic) IBOutlet UILabel *currencyB;
+@property (weak, nonatomic) IBOutlet UILabel *currencyC;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (IBAction)buttonTapped:(id)sender {
+    self.convertButton.enabled = NO;
+    
+    self.req = [[CRCurrencyRequest alloc] init];
+    self.req.delegate = self;
+    [self.req start];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)currencyRequest:(CRCurrencyRequest *)req
+    retrievedCurrencies:(CRCurrencyResults *)currencies;{
+    self.convertButton.enabled = YES;
+    
+    
+    
+    double inputValue = [self.inputField.text floatValue];
+    
+    double euroValue = inputValue * currencies.EUR;
+    
+    NSString *finalEuro = [NSString stringWithFormat:@"%.2f", euroValue];
+    
+    self.currencyA.text = finalEuro;
+    
+    
+    
+    
+    double yenValue = inputValue * currencies.JPY;
+    
+    NSString *finalYen = [NSString stringWithFormat:@"%.2f", yenValue];
+    
+    self.currencyB.text = finalYen;
+    
+    
+    
+    
+    double poundValue = inputValue * currencies.GBP;
+    
+    NSString *finalPound = [NSString stringWithFormat:@"%.2f", poundValue];
+    
+    self.currencyC.text = finalPound;
+    
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [self.view endEditing:YES];
 }
 
 @end
